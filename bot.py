06a0dab9bd11689496625bcda2d973a443ae0f37
Ariez <kritamet.cho@gmail.com>
2025-09-13 06:01:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,6 +52,11 @@ ytdl_opts = {
     'noplaylist': True,
     'quiet': False,
     'default_search': 'ytsearch',
+    'nocheckcertificate': True,
+    'source_address': '0.0.0.0',
+    'headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
+    },
     'cookiefile': 'cookies.txt'
 }
 
